913e3be5c8077d8a01d6fe94baff7cc975eee965
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-01-27 18:41:42 -0500

    Finish C

---
+++ stableMatching/shuffleedMatching.py
@@ -0,0 +1,98 @@
+import argparse
+import random
+import copy
+def main(args):
+    num = args.size
+
+    # initialize the preferences of men and women, depending on if the user choose the worstcase option
+    if args.worstcase:
+        menPref = [
+            [0,1,2,3,4],
+            [1,2,3,0,4],
+            [2,3,0,1,4],
+            [3,0,1,2,4],
+            [0,1,2,3,4]
+        ]
+        womenPref = [
+            [1,2,3,4,0],
+            [2,3,4,0,1],
+            [3,4,0,1,2],
+            [4,0,1,2,3],
+            [0,1,2,3,4]
+        ]
+        num = 5
+    else:
+        menPref = []
+        womenPref = []
+        
+        for _ in range(num):
+            arr = [n for n in range(num)]
+            random.shuffle(arr)
+            menPref.append(arr)
+            womenPref.append(arr.copy())
+        random.shuffle(menPref)
+        random.shuffle(womenPref)
+    
+    unstable = 0
+    stable = 0
+    for i in range(1000):
+        # Deep copy to make sure changes in basicGaleShapley does not affect menPref in main
+        result = basicGaleShapley(copy.deepcopy(menPref), womenPref)
+
+        if result is dict():
+            unstable += 1
+        else:
+            stable += 1
+        
+        for i in range(num):
+            random.shuffle(menPref[i])
+            random.shuffle(womenPref[i])
+        
+    
+    print('Percentage of stable matching is: {percentage:.2f}%'.format(percentage=float(stable/1000)*100))
+
+
+    
+
+
+def basicGaleShapley(menPref, womenPref):
+    # The set stores unmatched men and the dictionary stores temporary spouse of women
+    unmatchedMan = set()
+    spouseOfWoman = dict()
+    for i in range(len(menPref)):
+        unmatchedMan.add(i)
+        spouseOfWoman[i] = None
+
+    while unmatchedMan:
+        # Pop a unmatchedman to propose
+        proposeMan = unmatchedMan.pop()
+
+        # If error happen
+        if not menPref[proposeMan]:
+            return dict()
+
+        # Prepared to proposed to his top rated woman
+        proposedWoman = menPref[proposeMan].pop(0)
+
+        # She is unmatched
+        if spouseOfWoman[proposedWoman] is None:
+            spouseOfWoman[proposedWoman] = proposeMan
+        # This man is her better choice
+        elif womenPref[proposedWoman].index(spouseOfWoman[proposedWoman]) > womenPref[proposedWoman].index(proposeMan):
+            unmatchedMan.add(spouseOfWoman[proposedWoman])
+            spouseOfWoman[proposedWoman] = proposeMan
+        # Rejected
+        else:
+            unmatchedMan.add(proposeMan)
+
+    return spouseOfWoman
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test for argparse')
+    parser.add_argument('--size', '-s', default=8, help='the size of the matching')
+    parser.add_argument('--worstcase', '-w', default=False, help='if use the worst case as input, size = 4')
+    args = parser.parse_args()
+    main(args)
